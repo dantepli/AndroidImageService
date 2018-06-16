8b826d197ddc05080e7a75de0c55e9850c396dd8
dantepli <dan932@gmail.com>
2018-06-16 23:37:51 +0300

    Added image service template. Toast messages.

---
+++ app/src/main/java/bardan/androidimageservice/ImageServiceService.java
@@ -0,0 +1,33 @@
+package bardan.androidimageservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class ImageServiceService extends Service {
+
+    public ImageServiceService() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flag, int startId) {
+        Toast.makeText(this, "Service Starting...", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        Toast.makeText(this, "Service ending...", Toast.LENGTH_SHORT).show();
+    }
+}
