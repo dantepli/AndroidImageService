cf5c94dab7b10f27d2e09f9c1ee4f3aae848be79
dantepli <dan932@gmail.com>
2018-06-18 13:26:47 +0300

    Broadcast reciever added.

--- app/src/main/java/bardan/androidimageservice/ImageServiceService.java
+++ app/src/main/java/bardan/androidimageservice/ImageServiceService.java
@@ -1,12 +1,20 @@
 package bardan.androidimageservice;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class ImageServiceService extends Service {
+
+    private BroadcastReceiver wifiBroadcast;
 
     public ImageServiceService() {
     }
@@ -20,6 +28,29 @@ public class ImageServiceService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        final IntentFilter wifiFilter = new IntentFilter();
+        wifiFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        wifiFilter.addAction("android.net.wifi.STATE_CHANGE");
+        this.wifiBroadcast = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        //get the different network states
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            startImageTransfer();
+                        }
+                    }
+                }
+            }
+        };
+        this.registerReceiver(this.wifiBroadcast, wifiFilter);
+    }
+
+    private void startImageTransfer() {
+
     }
 
     public int onStartCommand(Intent intent, int flag, int startId) {
